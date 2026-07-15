a00fcae6b81d197396b2619d4ce7c708ce7b8394
Ankit Negi <ankitnegi240204@Ankits-MacBook-Air.local>
2026-07-15 11:26:27 +0530

    hemlo

--- fifth.java
+++ fifth.java
@@ -2,6 +2,7 @@ import java.util.*;
  public class fifth {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("hello");
         System.out.print("Enter the number of rows: ");
         int n = sc.nextInt();
         int a=0;
